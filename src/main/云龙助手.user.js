855cee0037215b3866adc98c4df06e249cc79515
Dave.Wu <wugenwei2010@163.com>
2022-05-14 18:35:37 +0800

    Update 云龙助手.user.js

--- src/main/云龙助手.user.js
+++ src/main/云龙助手.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
-// @name         云龙助手
+// @name         环境助手
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://xxx.aaa.com/*
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
 // @require      http://libs.baidu.com/jquery/1.9.1/jquery.min.js
-// @grant        unsafeWindow
+
+// @match https://*/abc/*
+
 // ==/UserScript==
 
 function addGlobalStyle(css) {
@@ -24,19 +28,21 @@ function addGlobalStyle(css) {
 
     addGlobalStyle(`
 .fixed_area{
-position:fixed;
-top: 33%;
-right: 40px;
-font-weight: bold;
-color: mediumblue;
-font-size: 16px;
+    position: fixed;
+    top: 2%;
+    right: 240px;
+    font-weight: bold;
+    color: mediumblue;
+    font-size: 16px;
+    z-index: 1050;
 }
-
+.fixed_area span{
+   background-color: yellow;
+    cursor: pointer;
+}
 .fixed_area ul{
 line-height: 30px;
-
 }
-
 .fixed_area li{
 padding: 5px 15px;
 list-style:none;
@@ -44,29 +50,23 @@ background-color: gold;
 margin-top:6px;
 opacity:0.5;
 transition:all .5s;
-box-shadow: 5px 5px 5px orange;
+box-shadow: 3px 3px 3px orange;
 position: relative;
 }
-
 .fixed_area li:hover{
 opacity:1;
 text-decoration: underline;
 transform:translateX(-15px);
 }
-
 .fixed_area li.selected{
 opacity:1;
 border: 2px solid red;
+border-radius:5px;
 }
-
 `)
 
+    //alert(0)
 
-    window.onload = function () {
-
-        detectYunlong();
-
-    };
 
 
     var yunlongList = [
@@ -77,40 +77,43 @@ border: 2px solid red;
         {
             name:"bbb",
             url:"https://bbb"
-        },
-        {
-            name:"ccc",
-            url:"https://ccc"
-        },
-        {
-            name:"ddd",
-            url:"https://ddd"
         }
     ];
+
+    setTimeout(function(){
+        detectYunlong();
+    },0)
+
 
     function detectYunlong(){
         var _currentUrl = window.location.href;
 
         var liArray="";
 
-        for(let y of yunlongList){
+        yunlongList.forEach(function(y){
             let _name = y.name;
             let _url = y.url;
             var current=`<li><a href='${_url}'>${_name}</a></li>`;
-            if(_currentUrl == _url){
+
+            if(_currentUrl == _url || _currentUrl.startsWith(_url)){
                 current=`<li class='selected'><a href='${_url}'>${_name}</a></li>`;
             }
             liArray+=current
-        }
+        })
 
         var fix = $(`<div id='dave' class='fixed_area'>
-<ul>
+<div>
+<span id="daveFast">助手<span>
+</div>
+<ul id="daveUl" style="display:none">
 ${liArray}
 </ul>
 </div>`);
         $("body").append(fix);
 
-
+        $("#daveFast").click(function(){
+            $("#daveUl").toggle();
+        })
     };
 
 })();
